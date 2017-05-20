cff1b7a8942b75af1b7a56476c864984b81f9e19
schwindelig <david.szoeke@gmail.com>
2017-05-20 20:56:49 +0200

    Implement baseline

---
+++ src/Exhibitor/Exhibitor.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections.Generic;
+using System.Reflection;
+
+namespace Exhibitor
+{
+    public static class Exhibitor
+    {
+        public static ExhibitorProxy<T> Load<T>()
+        {
+            return Load(default(T));
+        }
+
+        public static ExhibitorProxy<T> Load<T>(T instance)
+        {
+            var typeInfo = typeof(T).GetTypeInfo();
+
+            return new ExhibitorProxy<T>(
+                instance,
+                LoadFields(typeInfo),
+                LoadProperties(typeInfo),
+                LoadMethods(typeInfo));
+        }
+
+        private static IEnumerable<FieldInfo> LoadFields(TypeInfo typeInfo)
+        {
+            return typeInfo.GetFields(BindingFlags.Instance | BindingFlags.NonPublic | BindingFlags.Public);
+        }
+
+        private static IEnumerable<PropertyInfo> LoadProperties(TypeInfo typeInfo)
+        {
+            return typeInfo.GetProperties(BindingFlags.Instance | BindingFlags.NonPublic | BindingFlags.Public);
+        }
+
+        private static IEnumerable<MethodInfo> LoadMethods(TypeInfo typeInfo)
+        {
+            return typeInfo.GetMethods(BindingFlags.Instance | BindingFlags.NonPublic | BindingFlags.Public);
+        }
+    }
+}
